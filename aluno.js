bd299f70a74e3ad2205e4d2b934ead38dba774f7
LEONARDO SOUZA DA CONCEICAO <557392044@alunos.etec.itaqua>
2019-09-06 15:23:56 -0300

    Foi realizado requisição assincrona com servidor API

--- aluno.js
+++ aluno.js
@@ -1,17 +1,22 @@
-window.onload=  function() {
-console.warn("OLAAAA MUNDO")
+window.onload = function () {
+   console.warn("OLAAAA MUNDO")
 
    let meuForm = $("form#formAluno")
 
    console.log(meuForm)
 
-   meuForm.on("submit", function(event){
-event.preventDefault()
-console.log("Deu certo")
-let botao = $("form button")
-console.log(botao)
+   meuForm.on("submit", function (event) {
+      event.preventDefault()
+      console.log("Deu certo")
+      let botao = $("form button")
+      console.log(botao)
 
-let botaoQGira = Ladda.create(botao[0])
-botaoQGira.start()
-})
+      let botaoQGira = Ladda.create(botao[0])
+      botaoQGira.start()
+      fetch("http://localhost:8080/sen-api/teste.php").then(function (resposta) {
+         return resposta.json()
+      }).then(function (dados) {
+         alert(dados.mensagem)
+      })
+   })
 }
